9ef9debf1674f09f8b0ffc829bff09fdbee19e1b
Ethan Peters <et8lg2pz@students.codeinstitute.net>
2024-12-21 12:14:34 +0000

    Create the Flask App

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env # noqa: F401
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"), 
+            port=int(os.environ.get("PORT")),
+            debug=False)
